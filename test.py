2f5a8acd4b7ddff4419231a8134f26978a89dd43
Cathal Crowe <cathalcrowepatrick@gmail.com>
2026-02-10 15:55:48 +0000

    20 Amino Acid Rule Check

--- test.py
+++ test.py
@@ -1,7 +1,22 @@
 from Bio import SeqIO
 
+
+standard_aa = set("ACDEFGHIKLMNPQRSTVWY")  # standard amino acids
+standard_count = 0
+non_standard_count = 0
+
 for record in SeqIO.parse("data/uniprot_sprot.fasta", "fasta"):
-    print(f"ID: {record.id}")
-    print(f"Sequence: {record.seq}")
-    print(f"Length: {len(record)}")
-    print(f"Description: {record.description}")
+    if not set(record.seq).issubset(standard_aa):
+        print(f"Non-standard sequence found in record {record.id}")
+        print(f"Sequence: {record.seq}")
+        print(f"Length: {len(record)}")
+        print(f"Description: {record.description}")
+        non_standard_count += 1
+    else:
+        print(f"Standard sequence found in record {record.id}")
+        print(f"Sequence: {record.seq}")
+        print(f"Length: {len(record)}")
+        print(f"Description: {record.description}")
+        standard_count += 1
+print(f"Total standard sequences: {standard_count}")
+print(f"Total non-standard sequences: {non_standard_count}")
