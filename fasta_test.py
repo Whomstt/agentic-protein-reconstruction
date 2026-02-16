9c6a8b6a04205274c0dfb5d542a0efb57dcf1aa2
Cathal <cathalcrowepatrick@gmail.com>
2026-02-16 13:08:10 +0000

    ProtBERT masked language modeling experiment

--- fasta_test.py
+++ fasta_test.py
@@ -1,10 +1,11 @@
 from Bio import SeqIO
 
 
-standard_aa = set("ACDEFGHIKLMNPQRSTVWY")  # standard amino acids
+standard_aa = set("ACDEFGHIKLMNPQRSTVWY")  # standard 20 amino acids
 standard_count = 0
 non_standard_count = 0
 
+# Count the number of standard and non-standard sequences in the FASTA file
 for record in SeqIO.parse("data/uniprot_sprot.fasta", "fasta"):
     if not set(record.seq).issubset(standard_aa):
         print(f"Non-standard sequence found in record {record.id}")
